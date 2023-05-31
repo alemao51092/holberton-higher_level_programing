fe5f2930ce21b071f10bfbb765dd1200b88b9646
alemao51092 <aarielmartinez1111@gmail.com>
2023-05-31 10:46:10 -0700

    fixing errors

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for number in range(0, 101):
-        if number % 5 == 0:
-            print("Buzz", end=" ")
+    "A function that prints 1 to 100"
+    for number in range(1, 101):
+        if number % 15 == 0:
+            print("FizzBuzz", end=' ')
         elif number % 3 == 0:
-            print("Fizz", end=" ")
-        elif number % 15 == 0:
-            print("FizzBuzz", end=" ")
+            print("Fizz", end=' ')
+        elif number % 5 == 0:
+            print("Buzz", end=' ')
         else:
-            print("{:d}".format(number), end=" ")
+            print("{:d}".format(number), end=' ')
