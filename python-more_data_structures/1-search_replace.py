96b3fec7792eb231dc56e27cbbed9d800f0c7d47
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-07 09:24:08 -0700

    fixing errors

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     newlist = list(my_list)
     for idx in range(len(my_list)):
-        if idx == search:
+        if newlist[idx] == search:
             newlist[idx] = replace
     return newlist
