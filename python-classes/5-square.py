23f3d8d484fad95a8c7d90cf2649722209670b35
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-12 11:05:34 -0700

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""Write class Square that defines a square and the size"""
+
+
+class Square:
+    """ Define a square """
+
+    def __init__(self, size=0):
+
+        """ Instantiation """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+
+        """ Calculet the area"""
+
+        squarearea = self.__size * self.__size
+        return squarearea
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
