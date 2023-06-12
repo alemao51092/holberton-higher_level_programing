0e68c9a412cf211b6ddcc4a044a5fe94722dae82
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-12 09:14:11 -0700

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Write class Square that defines a square and the size"""
+
+
+class Square:
+    """ Define a square """
+    def __init__(self, size):
+        """ Instantiation """
+        self.__size = size
