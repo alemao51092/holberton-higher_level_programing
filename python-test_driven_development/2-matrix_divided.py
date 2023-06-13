b02172f54a18d1b2836421123fb8dbbbf475e3a7
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-13 11:10:57 -0700

    fixing a ton of errors

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,23 +3,23 @@
 
 
 def matrix_divided(matrix, div):
-    typ = TypeError
-    zerr = ZeroDivisionError
+    """divide matrix"""
     err = "matrix must be a matrix (list of lists) of integers/floats"
-    if not isinstance(div, (int, float)):
-        raise typ("div must be a number")
-    if div == 0:
-        raise zerr("division by zero")
-
-    first_row_length = len(matrix[0])
-    div_matrix = []
+    divmatrix = []
+    firstrowlenght = len(matrix[0])
     for row in matrix:
-        if len(row) != first_row_length:
-            raise typ("Each row of the matrix must have the same size")
-        if any(not isinstance(item, (int, float)) for item in row):
-            raise typ(err)
-
-        div_row = [round(element / div, 2) for element in row]
-        div_matrix.append(div_row)
-
-    return div_matrix
+        if len(row) != firstrowlenght:
+            raise TypeError("Each row of the matrix must have the same size")
+        for item in row:
+            if not isinstance(item, (int, float)):
+                raise TypeError(err)
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if div is None:
+        raise TypeError("missing 1 required positional argument: 'div'")
+    for row in matrix:
+        divrow = [round(element / div, 2) for element in row]
+        divmatrix.append(divrow)
+    return divmatrix
