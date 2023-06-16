1ae8630287864388e0ae30f224c470c46bbc061c
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-16 10:35:56 -0700

    fixing errors

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -6,6 +6,16 @@ class Rectangle:
     """ A rectangle with a width and height. """
 
     def __init__(self, width=0, height=0):
+        """inicialization"""
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        if width < 0:
+            raise ValueError("width must be >= 0")
 
         self.width = width
         self.height = height
