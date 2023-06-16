9cffd051e4456f14e8b8e2d259ed73f544efeddb
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-16 09:14:17 -0700

    task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+class Rectangle:
+    """ A rectangle with a width and height. """
+
+    def __init__(self, w, h):
+        """ (Rectangle, number, number)"""
+
+        self.width = w
+        self.height = h
