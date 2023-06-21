4d2e1b889ef841851983be3e4ddfe111df0749c1
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-21 10:19:36 -0700

    task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+class BaseGeometry:
+    """define a class"""
+    pass
+
+    def area(self):
+
+        """area"""
+
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        
+        if type(value)is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError( name + " must be greater than 0")
+        self.name = name
+        self.value = value
