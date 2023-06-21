226096d38ef381b2477aaa6e26986f899a9da3c8
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-21 09:49:14 -0700

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+class BaseGeometry:
+    """define a class"""
+    pass
+
+
+def area(self):
+    """area"""
+    raise Exception("area() is not implemented")
