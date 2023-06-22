3fbe16c1900bc6b968c68ec0436c7cf49279f162
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-22 11:27:58 -0700

    task1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """write a string in a file"""
     with open(filename, mode="w", encoding="utf-8") as f:
         f.write(text)
         return len(text)
