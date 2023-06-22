24bc9fea0d3c40502b20692b8f9be8205a4dfaca
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-22 09:23:39 -0700

    task11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""shebang"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a sub class square of the base class rectangle"""
+
+    def __init__(self, size):
+        """square"""
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """return area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
