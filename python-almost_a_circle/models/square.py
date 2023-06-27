69161171c0d1b03815acf8f724dc4eba96e7c5dc
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-27 11:22:47 -0700

    task10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""shebang"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    "sub class Square"
+
+    def __init__(self, size, x=0, y=0, id=None):
+        "instantation"
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        "String representation of the square"
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - "
+                f"{self.height}")
