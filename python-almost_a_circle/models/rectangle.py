1b04d7a4b13cd89e9527f525dbc67d6df542f575
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-27 10:13:22 -0700

    task 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,8 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print("")
+
+    def __str__(self):
+        "String representation of the rectangle"
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+                f"{self.__width}/{self.__height}")
