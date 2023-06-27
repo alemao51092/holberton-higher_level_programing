f82fc2e2095029e8e158f8a160cab606119d57a4
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-27 06:30:02 -0700

    fixing style

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """shebang"""
 
+
 class Base:
     """class"""
-    
+
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
