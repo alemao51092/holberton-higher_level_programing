01f5b8b98ade34a383f34e7e527d6f62e48a9912
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-28 11:25:08 -0700

    task15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """shebang"""
 
 
+import json
+
+
 class Base:
     """class"""
 
@@ -13,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        "Returns the JSON representation of list_dictionaries"
+        if list_dictionaries is None or list_dictionaries is len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
