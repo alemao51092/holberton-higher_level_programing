43b7c0bf044db4e14e6b229e8e458cd5cd9bf3ec
alemao51092 <aarielmartinez1111@gmail.com>
2023-06-28 12:05:10 -0700

    task17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -37,3 +37,10 @@ class Base():
                 json_list.append(obj.to_dictionary())
         with open(filename, 'w') as file:
             file.write(cls.to_json_string(json_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        "returns the list of the JSON string"
+        if json_string is None or json_string == 0:
+            return []
+        return json.loads(json_string)
